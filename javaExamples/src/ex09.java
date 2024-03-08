9c80ac6ebc94c10c633403cac3cd75930dc6c8fc
DESKTOP-SERQG8D\jungr <jungreul97>
2024-03-08 11:32:37 +0900

    형변환 연습

--- javaExamples/src/ex09.java
+++ javaExamples/src/ex09.java
@@ -18,13 +18,26 @@ public class ex09 {
         System.out.println(k%100/10);
         //3. 백의 자릿수 1을 출력하시오.
         System.out.println(k/100);
+
         double d = 3.567;
         //4. 소숫점 첫째자리를 출력하시오.
+        // * 소수점 없애기
+        // 1. Math.floor
+        // 2. (int) 형변환
         System.out.println((int)(d%1/0.1));
+        System.out.println(Math.floor(d*10%10));
         //5. 소숫점 첫째자리에서 반올림하여 출력하시오.
         System.out.printf("%.0f",d);
-        //6. 소숫점 둘째자리에서 반올림하여 출력하시오.
         System.out.println();
+        System.out.println(Math.round(d));
+        // Math.round 는 무조건 소수점 첫째자리에서만 반올림 함
+        // 1. 0.5를 더해준다
+        // 2. (int)형변환연산자로 소수점을 날린다.
+        // 3. 예) 3.49 => 3 +0.5 (int)3.99 => 3 Math.round 안에서 동작하는 원리
+        //        3.5  => 4 +0.5 (int)4.0 => 4
+        //        3.99 => 4
+        //6. 소숫점 둘째자리에서 반올림하여 출력하시오.
+        System.out.println(Math.round(d*10)/10.0);
         System.out.printf("%.1f",d);
     }
 }
