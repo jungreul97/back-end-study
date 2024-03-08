5eef72b589e0e7ba6684a414d88599b42fe6cd7d
DESKTOP-SERQG8D\jungr <jungreul97>
2024-03-08 10:26:38 +0900

    단항 연산자 개념

---
+++ javaExamples/src/ex08.java
@@ -0,0 +1,22 @@
+public class ex08 {
+    public static void main(String[] args) {
+        // 단항 : ++ -- !(논리반전) (타입) ~(비트반전)
+        int i = 10;
+        i ++ ;
+        System.out.println(i); // 11
+        i--;
+        System.out.println(i); // 10
+
+        boolean b = true;
+        System.out.println(!b); // false
+        System.out.println(!!b); // true
+
+        int j = 20;
+        short s = (short)j; // 작은 타입 <= 큰 타입 : 강제형변환
+
+        int k = 10; // 32bit 000000.....1010
+        int m = ~k; //       111111.....0101
+        System.out.println(Integer.toBinaryString(k)); // 1010 앞의 0은 생략되어서 나옴
+        System.out.println(Integer.toBinaryString(m)); // 11111111111111111111111111110101
+    }
+}
