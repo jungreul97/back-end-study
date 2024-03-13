1ca7c12ac66c3b0242c8f48546c893e71c5c0140
DESKTOP-SERQG8D\jungr <jungreul97>
2024-03-13 11:32:04 +0900

    final 정리

--- javaExamples/src/ex41.java
+++ javaExamples/src/ex41.java
@@ -1,4 +1,7 @@
 // final이 클래스 안에서 사용될 때
+// 1. fianl 필드 : 상수처럼 동작, 한번 값이 들어가면 다시 대입 안됨.
+// 2. final class : 상속 불가
+// 3. final 메소드 : 오버라이드 불가
 final class FinalClass{
     String name = "파이널 클래스";
     final void disp(){} // 자식 클래스에 오버라이드도 안됨!!!
