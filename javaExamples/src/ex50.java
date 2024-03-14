1bc64bd77a8a999ec0c515e690aab6606158bb56
DESKTOP-SERQG8D\jungr <jungreul97>
2024-03-14 10:30:23 +0900

    중첩 클래스

--- javaExamples/src/ex50.java
+++ javaExamples/src/ex50.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ex50 {
+// 중첩 클래스 : 클래스 안에 클래스가 들어가는 것
+// 외부(Outer) 클래스 : 클래스 밖에 클래스를 선언
+// 내부(Inner) 클래스 : 클래스 안에 클래스를 선언
+// 내부클래스를 사용하는 이유
+// : 내부적으로 소비하고 끝나는 클래스는 내부클래스로 선언
+class Outer{
+    String name = "outer";
+    class Inner {
+        String name = "inner";
+    }
+    static class InnerStatic{
+        String name = "inner static";
+    }
+}
+public class ex50 {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        System.out.println(outer.name);
+        Outer.Inner inner = outer.new Inner(); // Outer 객체가 내려와야 됨.
+        System.out.println(inner.name);
+        Outer.InnerStatic innerStatic = new Outer.InnerStatic();
+        System.out.println(innerStatic.name);
+    }
 }
