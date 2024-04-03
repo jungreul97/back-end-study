c6d1acdcce3980a78f6e27dbe5ef72a321eea5c3
DESKTOP-SERQG8D\jungr <jungreul97>
2024-04-03 10:07:47 +0900

    pr03vm 풀이

--- springboot_study/Pr03VM/src/main/java/com/study/Pr03VM/MainController.java
+++ springboot_study/Pr03VM/src/main/java/com/study/Pr03VM/MainController.java
@@ -15,6 +15,7 @@ public class MainController {
     @GetMapping("/")
     public String main(Model model){
         model.addAttribute("list", productlist);
+        model.addAttribute("count", productlist.size());
         return "productList";
     }
     @GetMapping("/addProductForm")
@@ -39,32 +40,38 @@ public class MainController {
         // main으로 다시 보내줌 대문초기화면으로 보내줌
         // 전달하면, 웹브라우저는 받자마자 이경로로 다시 요청함.
     }
+    // editProductForm?index=0
     @GetMapping("/editProductForm")
     public String editProductForm(@RequestParam("index") int index,Model model){
         Product product = productlist.get(index);
         model.addAttribute("product", product);
-        model.addAttribute("index",index);
+        model.addAttribute("index",index); // index도 같이 정보를 전달해줘야된다 editform에서 받으려면
         return "editProductForm";
     }
     @PostMapping("/editProduct")
-    public String editProduct(@RequestParam("index") int index, @RequestParam(required = false, defaultValue = "") String inputName, @RequestParam int inputPrice, @RequestParam LocalDate inputLimitDate){
+    @ResponseBody
+    public String editProduct(@RequestParam int index, @RequestParam(required = false, defaultValue = "") String inputName, @RequestParam int inputPrice, @RequestParam LocalDate inputLimitDate){
         System.out.println(inputName);
         System.out.println(inputPrice);
         System.out.println(inputLimitDate);
+        // Product product = list.get(index);
+        // product.setName(inputName);
+        // product.setPrice(inputPrice);
+        // product.setLimit_date(inputLimitDate);
         Product product = Product.builder().name(inputName).price(inputPrice).limit_date(inputLimitDate).build();
         productlist.set(index,product);
         System.out.println("index : "+index);
         System.out.println("size : "+productlist.size());
-        return "redirect:/";
+        return "<script>alert('수정되었습니다.'); location.href='/';</script>";
 
     }
+    // deleteProduct?index=0
     @GetMapping("/deleteProduct")
+    @ResponseBody
     public String deleteProduct(@RequestParam("index") int index){
         productlist.remove(index);
         System.out.println("index : "+index);
         System.out.println("size : "+productlist.size());
-        return "redirect:/";
+        return "<script>alert('상품이 삭제되었습니다.'); location.href='/';</script>";
     }
-
-
 }
